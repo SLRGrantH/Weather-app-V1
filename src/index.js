41f353539c9d269d082c137aaf9335948aefa738
SLRGH <sarah.l.r.grant@gmail.com>
2022-02-26 15:46:22 +0000

    text description V4
    Added [0]

--- src/index.js
+++ src/index.js
@@ -101,7 +101,7 @@ function showTemperature(response) {
   let currentSummaryDescription = document.querySelector(
     "#current-summary-description"
   );
-  currentSummaryDescription.innerHTML = response.data.weather.description;
+  currentSummaryDescription.innerHTML = response.data.weather[0].description;
 }
 
 /*function showDescription(response) {
